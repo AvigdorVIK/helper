d3118d519ee7a8292aecad347fb9a5fce2e710a0
AvigdorVIK <kanynnikovvik19@gmail.com>
2022-03-15 20:37:42 +0200

    Додавання Шаблонів

--- helpus/views.py
+++ helpus/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+menu = ['Про нас','Наша допомога','Моя потреба','Увійти']
+
 def home(request):
-    return render(request ,'helpus/home.html')
+    return render(request ,'helpus/home.html', {'menu': menu, 'title': 'Головна'})
+
+
+def index(request):
+    return render(request ,'helpus/index.html', {'menu': menu, 'title': 'Про нас'})
