9ebcd8d7203d2fde3824f109efdc3c7b90c4b050
AvigdorVIK <kanynnikovvik19@gmail.com>
2022-03-16 13:57:11 +0200

    models

--- helpus/models.py
+++ helpus/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Helpus(models.Model):
+    titles = models.CharField(max_length=150)
+    text = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
